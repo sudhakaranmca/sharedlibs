fc975f9e110d7542eac24ca862a5d5d8151bccb2
sudhakaranmca <sudhakaran_mca@yahoo.com>
2021-08-17 20:58:56 +0530

    Create calculator.groovy

---
+++ vars/calculator.groovy
@@ -0,0 +1,20 @@
+def add(a,b)
+{
+  println " Added value of given two arguments is ${a+b}" 
+}
+
+def mul(a,b)
+{
+  println " Multiplied value of given two arguments is ${a*b}" 
+}
+
+def div(a,b)
+{
+  println " Divided value of given two arguments is ${a/b}" 
+}
+
+def sub(a,b)
+{
+  println " Subtracted value of given two arguments is ${a-b}" 
+}
+
